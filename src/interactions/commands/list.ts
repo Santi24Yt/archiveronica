c39e2b7b04d8e33a2306ac778d20a18fb051094f
Santi_24Yt <santi24yt@gmail.com>
2023-01-14 21:47:20 -0600

    add avatar to command list, overwriteCommands, github actions

--- src/interactions/commands/list.ts
+++ src/interactions/commands/list.ts
@@ -1,6 +1,7 @@
 import type Command from "../Command.js";
+import avatar from "./avatar.js";
 import ping from "./ping.js";
 
-const commandList: Command[] = [ping];
+const commandList: Command[] = [ping, avatar];
 
 export default commandList;
---
+++ src/overwriteCommands.ts
@@ -0,0 +1,43 @@
+import { config } from "dotenv";
+import commandList from "./interactions/commands/list.js";
+import type Command from "./interactions/Command.js";
+
+config();
+
+if (process.env.APP_ID === undefined || process.env.APP_ID === "") {
+  throw new Error("No app id provided");
+}
+if (
+  process.env.DISCORD_TOKEN === undefined ||
+  process.env.DISCORD_TOKEN === ""
+) {
+  throw new Error("No discord token provided");
+}
+
+const commands: Command[] = commandList;
+
+try {
+  const overwrite = await fetch(
+    "https://discord.com/api/v10/" +
+      `applications/${process.env.APP_ID}/commands?with_localizations=true`,
+    {
+      method: "PUT",
+      body: JSON.stringify(commands.map((cmd) => cmd.json)),
+      headers: {
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  if (!overwrite.ok) {
+    const responseText = await overwrite.text();
+    throw new Error(`${overwrite.status} ${responseText}`);
+  }
+  await overwrite.json();
+  console.log(overwrite.body);
+} catch (error) {
+  console.error("Unexpected error uploading commands");
+  console.error(error);
+}
+console.log("Commands overwritten successfully");
