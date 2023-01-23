279ebdaa8e0738b7cb0a6950a28e8d4b623e5fbd
Santi_24Yt <santi24yt@gmail.com>
2023-01-22 23:12:23 -0600

    ytdl command

--- src/interactions/commands/ytdl.ts
+++ src/interactions/commands/ytdl.ts
@@ -1,81 +1,78 @@
-/* eslint-disable @typescript-eslint/naming-convention */
+import { spawn } from "node:child_process";
+import { PassThrough } from "node:stream";
 import type { APIApplicationCommandInteractionDataBasicOption } from "discord-api-types/v10";
 
-import YTDlpWrap from "yt-dlp-wrap";
 import type Command from "../Command.js";
 
-// eslint-disable-next-line new-cap
-const ytdlp = new YTDlpWrap.default();
-
-interface VideoInfo {
-  id: string;
-  title: string;
-  fulltitle: string;
-  ext: string;
-  alt_title: string;
-  description: string;
-  display_id: string;
-  uploader: string;
-  license: string;
-  creator: string;
-  timestamp: number;
-  upload_date: string;
-  release_timestamp: number;
-  release_date: string;
-  modified_timestamp: number;
-  modified_date: string;
-  uploader_id: string;
-  channel: string;
-  channel_id: string;
-  channel_follower_count: number;
-  location: string;
-  duration: number;
-  duration_string: string;
-  view_count: number;
-  concurrent_view_count: number;
-  like_count: number;
-  dislike_count: number;
-  repost_count: number;
-  average_rating: number;
-  comment_count: number;
-  age_limit: number;
-  live_status:
-    | "is_live"
-    | "is_upcoming"
-    | "not_live"
-    | "post_live"
-    | "was_live";
-  is_live: boolean;
-  was_live: boolean;
-  playable_in_embed: string;
-  availability:
-    | "needs_auth"
-    | "premium_only"
-    | "private"
-    | "public"
-    | "subscriber_only"
-    | "unlisted";
-  start_time: number;
-  end_time: number;
-  extractor: string;
-  extractor_key: string;
-  epoch: number;
-  autonumber: number;
-  video_autonumber: number;
-  n_entries: number;
-  playlist_id: string;
-  playlist_title: string;
-  playlist: string;
-  playlist_count: number;
-  playlist_index: number;
-  playlist_autonumber: number;
-  playlist_uploader: string;
-  playlist_uploader_id: string;
-  webpage_url: string;
-  webpage_url_basename: string;
-  webpage_url_domain: string;
-  original_url: string;
-}
+// aaainterface VideoInfo {
+//   id: string;
+//   title: string;
+//   fulltitle: string;
+//   ext: string;
+//   alt_title: string;
+//   description: string;
+//   display_id: string;
+//   uploader: string;
+//   license: string;
+//   creator: string;
+//   timestamp: number;
+//   upload_date: string;
+//   release_timestamp: number;
+//   release_date: string;
+//   modified_timestamp: number;
+//   modified_date: string;
+//   uploader_id: string;
+//   channel: string;
+//   channel_id: string;
+//   channel_follower_count: number;
+//   location: string;
+//   duration: number;
+//   duration_string: string;
+//   view_count: number;
+//   concurrent_view_count: number;
+//   like_count: number;
+//   dislike_count: number;
+//   repost_count: number;
+//   average_rating: number;
+//   comment_count: number;
+//   age_limit: number;
+//   live_status:
+//     | "is_live"
+//     | "is_upcoming"
+//     | "not_live"
+//     | "post_live"
+//     | "was_live";
+//   is_live: boolean;
+//   was_live: boolean;
+//   playable_in_embed: string;
+//   availability:
+//     | "needs_auth"
+//     | "premium_only"
+//     | "private"
+//     | "public"
+//     | "subscriber_only"
+//     | "unlisted";
+//   start_time: number;
+//   end_time: number;
+//   extractor: string;
+//   extractor_key: string;
+//   epoch: number;
+//   autonumber: number;
+//   video_autonumber: number;
+//   n_entries: number;
+//   playlist_id: string;
+//   playlist_title: string;
+//   playlist: string;
+//   playlist_count: number;
+//   playlist_index: number;
+//   playlist_autonumber: number;
+//   playlist_uploader: string;
+//   playlist_uploader_id: string;
+//   webpage_url: string;
+//   webpage_url_basename: string;
+//   webpage_url_domain: string;
+//   original_url: string;
+// }
 
 const ytdl: Command = {
   json: {
@@ -91,44 +88,109 @@ const ytdl: Command = {
       },
       {
         type: 5,
-        name: "audioOnly",
+        name: "audio-only",
         description: "only download audio",
       },
     ],
   },
+  // eslint-disable-next-line sonarjs/cognitive-complexity
   exec: async (ctx) => {
     const urlOption = ctx.findOption(
       "url"
     ) as APIApplicationCommandInteractionDataBasicOption;
 
+    const audioOption = ctx.findOption("audio-only") as
+      | APIApplicationCommandInteractionDataBasicOption
+      | undefined;
+
     ctx.defer();
 
     try {
-      const metadata: VideoInfo = (await ytdlp.getVideoInfo(
-        urlOption.value as string
-      )) as VideoInfo;
-
-      const vid = await ytdlp.execPromise([
-        "https://www.youtube.com/watch?v=aqz-KE-bpKQ",
+      const ytdlpOptions = [
+        urlOption.value as string,
+        "-o",
+        "-",
+        "--no-part",
+        "--no-playlist",
+        "--quiet",
         "-f",
-        "best[ext=mp4]",
-        "--max-filesize",
-        "8M",
-      ]);
+      ];
 
-      await ctx.editDeferred(
-        {
-          content: metadata.title,
-        },
-        [
+      if (audioOption !== undefined && (audioOption.value as boolean)) {
+        ytdlpOptions.push("worstaudio");
+      } else {
+        ytdlpOptions.push("worst[ext=mp4]");
+      }
+
+      const ytdlProcess = spawn("yt-dlp", ytdlpOptions);
+
+      // eslint-disable-next-line @typescript-eslint/no-misused-promises
+      ytdlProcess.on("error", async (error: unknown) => {
+        const errorMessage =
+          typeof error === "string" ? error : (error as Error).message;
+        await (errorMessage.includes(
+          'is not a valid URL. Set --default-search "ytsearch" (or run  yt-dlp "ytsearch'
+        )
+          ? ctx.editTextDeferred(
+              `${urlOption.value as string} is not a valid url`
+            )
+          : ctx.editTextDeferred(
+              `Unknown error ocurred, contact devs\nError: ${errorMessage}`
+            ));
+        console.error(error);
+      });
+
+      const stream = new PassThrough();
+      ytdlProcess.stdout.pipe(stream);
+
+      let stderrData = "";
+      ytdlProcess.stderr.on("data", (chunk) => {
+        stderrData += (chunk as Buffer).toString();
+      });
+
+      let file = Buffer.from("");
+      let fileSizeOnBytes = 0;
+
+      // 8 * 1024 * 1024 (8Mb)
+      const MAX_FILE_SIZE = 8_388_608;
+
+      stream.on("error", (error) => {
+        ytdlProcess.emit("error", error);
+      });
+
+      stream.on("data", (chunk) => {
+        fileSizeOnBytes += (chunk as Buffer).byteLength;
+        if (fileSizeOnBytes > MAX_FILE_SIZE) {
+          stream.emit("end");
+          return;
+        }
+        file = Buffer.concat([file, chunk]);
+      });
+
+      // eslint-disable-next-line @typescript-eslint/no-misused-promises
+      stream.on("end", async () => {
+        if (file.toString() === "" && stderrData !== "") {
+          ytdlProcess.emit("error", stderrData);
+          return;
+        }
+        stream.destroy();
+        ytdlProcess.kill();
+        await ctx.editDeferred(
           {
-            filename: "video.mp4",
-            value: Buffer.from(vid),
+            content: `<${urlOption.value as string}>`,
           },
-        ]
+          [
+            {
+              filename: "video.mp4",
+              value: file,
+            },
+          ]
+        );
+      });
+    } catch (error) {
+      await ctx.editTextDeferred(
+        `Unknown error ocurred, contact devs\nError: ${error as string}`
       );
-    } catch {
-      await ctx.editTextDeferred("Invalid url");
     }
   },
 };
