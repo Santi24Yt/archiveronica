1a1289420426adcb59849eeae58d513520209b79
Santi_24Yt <santi24yt@gmail.com>
2023-01-26 08:32:49 -0600

    ytdl reddit fix

--- src/interactions/commands/ytdl.ts
+++ src/interactions/commands/ytdl.ts
@@ -129,6 +129,7 @@ const ytdl: Command = {
       sortParameters += "+size,+filesize,+fs_approx,+res,+br";
 
       ytdlpOptions.push(sortParameters);
+      ytdlpOptions.push("--downloader-args '-f mp4 -movflags frag_keyframe+empty_moov'");
 
       const ytdlProcess = spawn("yt-dlp", ytdlpOptions);
 
